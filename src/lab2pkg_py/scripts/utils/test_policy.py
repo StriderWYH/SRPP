7deb9956395fea080c173e1a574b8f94a2856034
StriderWYH <2310428107@qq.com>
2023-06-16 23:08:49 -0700

    waiting for gazebo_env

--- src/lab2pkg_py/scripts/utils/test_policy.py
+++ src/lab2pkg_py/scripts/utils/test_policy.py
@@ -140,14 +140,15 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('fpath', type=str)
+    parser.add_argument('fpath', type=str,default= 'src/lab2pkg_py/scripts/data/sac/sac_s0')
     parser.add_argument('--len', '-l', type=int, default=0)
     parser.add_argument('--episodes', '-n', type=int, default=100)
-    parser.add_argument('--norender', '-nr', action='store_true')
+    parser.add_argument('--norender', '-nr', action='store_true',default= False)
     parser.add_argument('--itr', '-i', type=int, default=-1)
     parser.add_argument('--deterministic', '-d', action='store_true')
     args = parser.parse_args()
     env, get_action = load_policy_and_env(args.fpath, 
                                           args.itr if args.itr >=0 else 'last',
                                           args.deterministic)
+    ##print(env)
     run_policy(env, get_action, args.len, args.episodes, not(args.norender))
